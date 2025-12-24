2c7ddbac610a1485b85d71c0e6e1bd9353d42d65
Devin Le <devinle@Devins-MacBook-Pro-2.local>
2025-12-24 02:04:58 -0800

    add execute

--- lib/cpu_proc.cpp
+++ lib/cpu_proc.cpp
@@ -112,9 +112,46 @@ void execute_inc(Instruction inst) {
 void execute_dec(Instruction inst) {
     switch (inst.addr_mode) {
         case addr_mode::REG16: {
+            uint16_t reg_value = read_reg16(inst.reg_1);
+            uint16_t result = reg_value - 1;
+            write_reg16(inst.reg_1, result);
             break;
         }
         case addr_mode::REG8: {
+            uint8_t reg_value = read_reg8(inst.reg_1);
+            uint8_t result = reg_value - 1;
+
+            uint8_t f = read_reg8(reg_type::F);
+
+            if (result == 0) {
+                f |= FLAG_Z;
+            }
+
+            if (is_half_carry_sub(reg_value, 1)) {
+                f |= FLAG_H;
+            }
+
+            f |= FLAG_N;
+
+            write_reg8(reg_type::F, f);
+            write_reg8(inst.reg_1, result);
+            break;
+        }
+        case addr_mode::MEM_REG16: {
+            uint16_t addr = read_reg16(inst.reg_1);
+            uint8_t value = bus_read(addr);
+            uint8_t result = value - 1;
+            bus_write(addr, result);
+
+            uint8_t f = read_reg8(reg_type::F);
+            f |= FLAG_N;
+
+            if (result == 0) {
+                f |= FLAG_Z;
+            }
+
+            if (is_half_carry_sub(value, 1)) {
+                f |= FLAG_H;
             break;
         }
         default: {
@@ -127,15 +164,92 @@ void execute_dec(Instruction inst) {
 void execute_add(Instruction inst) {
     switch (inst.addr_mode) {
         case addr_mode::REG16_REG16: {
+            uint16_t reg_value_1 = read_reg16(inst.reg_1);
+            uint16_t reg_value_2 = read_reg16(inst.reg_2);
+            uint16_t result = reg_value_1 + reg_value_2;
+            write_reg16(inst.reg_1, result);
+
+            uint8_t f = read_reg8(reg_type::F);
+            f &= ~FLAG_N;
+
+            if (is_carry_add(reg_value_1, reg_value_2)) {
+                f |= FLAG_C;
+            }
+            if (is_half_carry_add(reg_value_1, reg_value_2)) {
+                f |= FLAG_H;
+            }
+
+            write_reg8(reg_type::F, f);
             break;
         }
         case addr_mode::REG8_REG8: {
+            uint8_t reg_value_1 = read_reg8(inst.reg_1);
+            uint8_t reg_value_2 = read_reg8(inst.reg_2);
+            uint8_t result = reg_value_1 + reg_value_2;
+            write_reg8(inst.reg_1, result);
+
+            uint8_t f = read_reg8(reg_type::F);
+            f &= ~FLAG_N;
+
+            if (result == 0) {
+                f |= FLAG_Z;
+            }
+
+            if (is_carry_add(reg_value_1, reg_value_2)) {
+                f |= FLAG_C;
+            }
+            if (is_half_carry_add(reg_value_1, reg_value_2)) {
+                f |= FLAG_H;
+            }
+
+            write_reg8(reg_type::F, f);
+            break;
+        }
+        case addr_mode::REG8_MEM_REG16: {
+            uint8_t reg_value = read_reg8(inst.reg_1);
+            uint16_t addr = read_reg16(inst.reg_2);
+            uint8_t value = bus_read(addr);
+            uint8_t result = reg_value + value;
+            write_reg8(inst.reg_1, result);
+
+            uint8_t f = read_reg8(reg_type::F);
+            f &= ~FLAG_N;
+
+            if (result == 0) {
+                f |= FLAG_Z;
+            }
+
+            if (is_carry_add(reg_value, value)) {
+                f |= FLAG_C;
+            }
+            if (is_half_carry_add(reg_value, value)) {
+                f |= FLAG_H;
+            }
+
+            write_reg8(reg_type::F, f);
             break;
         }
         case addr_mode::REG8_IMM8: {
-            break;
-        }
-        case addr_mode::REG16_IMM8: {
+            uint8_t reg_value = read_reg8(inst.reg_1);
+            uint8_t imm = fetch8();
+            uint8_t result = reg_value + imm;
+            write_reg8(inst.reg_1, result);
+
+            uint8_t f = read_reg8(reg_type::F);
+            f &= ~FLAG_N;
+
+            if (result == 0) {
+                f |= FLAG_Z;
+            }
+
+            if (is_carry_add(reg_value, imm)) {
+                f |= FLAG_C;
+            }
+            if (is_half_carry_add(reg_value, imm)) {
+                f |= FLAG_H;
+            }
+
+            write_reg8(reg_type::F, f);
             break;
         }
         default: {
